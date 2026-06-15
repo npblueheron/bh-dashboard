603b0d453e453b3e96c5283db2cdb761f23114ee
Naren Pendem <NPendem@blueheron.com>
2026-06-15 16:49:00 -0700

    fix redirect URI for mobile

--- src/services/authConfig.js
+++ src/services/authConfig.js
@@ -1,24 +1,16 @@
-// src/services/authConfig.js
 export const msalConfig = {
   auth: {
     clientId: "434f0a49-7cb8-4805-a62d-65a385169920",
     authority: "https://login.microsoftonline.com/7163abb0-3c0a-412b-b0c8-c9fdda5b1d3b",
-    redirectUri: window.location.origin
+    redirectUri: "https://thankful-pond-052fbeb0f.7.azurestaticapps.net",
+    postLogoutRedirectUri: "https://thankful-pond-052fbeb0f.7.azurestaticapps.net"
   },
   cache: {
     cacheLocation: "sessionStorage",
-    storeAuthStateInCookie: false
+    storeAuthStateInCookie: true
   }
 };
 
 export const loginRequest = {
   scopes: ["https://analysis.windows.net/powerbi/api/Dataset.Read.All"]
-};
-
-// Power BI workspace and dataset
-export const PBI_CONFIG = {
-  workspaceId: null, // will be discovered
-  datasetId: null,   // will be discovered
-  workspaceName: "WS_BH_DATA_DEV",
-  datasetName: "Project_Dashboard_Model"
 };
